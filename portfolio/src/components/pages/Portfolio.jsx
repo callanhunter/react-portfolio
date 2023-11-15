1b1bc23b149b20797029c5a71861bcbf56b1c872
callanhunter <callan57hunter@gmail.com>
2023-11-14 21:39:00 -0800

    adjusted buttons on projects page and added details to cloud projects

--- portfolio/src/components/pages/Portfolio.jsx
+++ portfolio/src/components/pages/Portfolio.jsx
@@ -7,8 +7,8 @@ import Api from "../images/api.png";
 import Employee from "../images/employee.png";
 import Readme from "../images/readme.png";
 import Ecommerce from "../images/ecommerce.png";
-import Cloud1 from "../images/cloudproject1.png";
-import Cloud2 from "../images/cloudproject8.png";
+import Cloud1 from "../images/cloudproject5.png";
+import Cloud2 from "../images/cloudproject2.png";
 
 export default function Portfolio() {
   return (
@@ -19,7 +19,7 @@ export default function Portfolio() {
             <h2 className=" text-center  h1 ">Projects</h2>
           </div>
         </div>
-        <div className="row row-cols-2 row-cols-md-2 g-4 mb-4 text-center">
+        <div className="row row-col row-cols-md-2 g-4 mb-4 text-center">
           <div className="col">
             <div className="card h-100">
               <div className="card-body">
@@ -50,24 +50,25 @@ export default function Portfolio() {
             </div>
           </div>
 
-          <div className="col">
+          <div className="col ">
             <div className="card h-100">
               <div className="card-body">
                 <h5 className="card-title text-center">Management System</h5>
                 <p className="card-title text-center">Back End Application</p>
                 <img src={Employee} className="card-img-top"></img>
-                <button type="button" className="btn btn-dark  mt-3">
+                <button type="button" className="btn btn-dark  mt-3 ">
                   <a
                     href="https://github.com/callanhunter/management-system"
-                    className="card-link text-white container mt-3 text-decoration-none"
+                    className="card-link text-white text-decoration-none gap-3"
                     target="_blank"
                   >
-                    GitHub link
+                    Deployed link
                   </a>
                 </button>
               </div>
             </div>
           </div>
+
           <div className="col">
             <div className="card h-100">
               <div className="card-body">
@@ -76,15 +77,64 @@ export default function Portfolio() {
                   Migrating a Database to Amazon RDS
                 </p>
                 <img src={Cloud1} className="card-img-top"></img>
-                <button type="button" className="btn btn-dark  mt-3">
-                  <a
-                    href="https://github.com/callanhunter/management-system"
-                    className="card-link text-white container mt-3 text-decoration-none"
-                    target="_blank"
+                <div className="container mt-3 p-4">
+                  <button
+                    type="button"
+                    className="btn btn-dark h-100 "
+                    data-bs-toggle="modal"
+                    data-bs-target="#myModal"
                   >
                     Details
-                  </a>
-                </button>
+                  </button>
+                </div>
+                <div className="modal" id="myModal">
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      {/* <!-- Modal Header --> */}
+                      <div className="modal-header">
+                        <h4 className="modal-title text-white">Details</h4>
+                        <button
+                          type="button"
+                          className="btn-close btn-dark"
+                          data-bs-dismiss="modal"
+                        ></button>
+                      </div>
+
+                      {/* <!-- Modal body --> */}
+                      <div className="card sub-card m-3 list-group-item-dark">
+                        <div className="card-body text-center">
+                          <h4 className="card-title">
+                            Migrating a Database to Amazon RDS
+                          </h4>
+                          <div className="container">
+                            <div className="row">
+                              Migrated a MariaDB database that runs on an EC2
+                              instance to a MariaDB database that runs on Amazon
+                              RDS, while updated to web application to use the
+                              new database for future use
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* <!-- Modal footer --> */}
+                      <div className="modal-footer">
+                        <div className="row">
+                          Skills: MariaDB · Amazon Relational Database Service
+                          (RDS) · Amazon EC2 · AWS Systems Manager · Amazon Web
+                          Services (AWS)
+                        </div>
+                        <button
+                          type="button"
+                          className="btn btn-dark h-100"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -96,15 +146,66 @@ export default function Portfolio() {
                   Creating a Dynamic Website for a Cafe
                 </p>
                 <img src={Cloud2} className="card-img-top"></img>
-                <button type="button" className="btn btn-dark  mt-1">
-                  <a
-                    href="https://github.com/callanhunter/management-system"
-                    className="card-link text-white container mt-1 text-decoration-none"
-                    target="_blank"
+
+                <div className="container mt-3 p-4">
+                  <button
+                    type="button"
+                    className="btn btn-dark h-100"
+                    data-bs-toggle="modal"
+                    data-bs-target="#myModal"
                   >
                     Details
-                  </a>
-                </button>
+                  </button>
+                </div>
+                <div className="modal" id="myModal">
+                  <div className="modal-dialog">
+                    <div className="modal-content">
+                      {/* <!-- Modal Header --> */}
+                      <div className="modal-header">
+                        <h4 className="modal-title text-white">Details</h4>
+                        <button
+                          type="button"
+                          className="btn-close btn-dark"
+                          data-bs-dismiss="modal"
+                        ></button>
+                      </div>
+
+                      {/* <!-- Modal body --> */}
+                      <div className="card sub-card m-3 list-group-item-dark">
+                        <div className="card-body text-center">
+                          <h4 className="card-title">
+                            Creating a Dynamic Website for a Cafe
+                          </h4>
+                          <div className="container">
+                            <div className="row">
+                              Deployed an application on an Amazon EC2 instance,
+                              enabling the cafe to accept online orders. This
+                              application works for the first AWS Region (the
+                              development environment) and the second AWS Region
+                              (the production environment) after creating an AMI
+                              from the EC2 instance
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      {/* <!-- Modal footer --> */}
+                      <div className="modal-footer">
+                        <div className="row">
+                          Skills: Amazon EC2 · Cloud9 IDE · AWS Systems Manager
+                          · Amazon Machine Image (AMI)
+                        </div>
+                        <button
+                          type="button"
+                          className="btn btn-dark"
+                          data-bs-dismiss="modal"
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
